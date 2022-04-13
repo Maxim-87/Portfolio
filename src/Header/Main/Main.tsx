7e7482f6346cab37e075b811688c787673b39b5a
Maxim87 <maksboks87@mail.ru>
2022-04-13 08:24:38 +0700

    changed color in Main.tsx add photo and text in Header

--- src/Header/Main/Main.tsx
+++ src/Header/Main/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './Main.module.scss';
 import styleContainer from '../../common/styles/Container.module.css'
+import photo from '../../assets/image/Photo (2).png'
 
 export function Main() {
     return (
@@ -9,9 +10,11 @@ export function Main() {
                 <div className={style.greet}>
                     <span> HI THERE </span>
                     <h1> I am MAXIM PECHYONKIN </h1>
-                    <p> Frontend developer </p>
+                    <p> Frontend developer with experience in creating SPA using React, Redux, TypeScript, JavaScript, SCSS, HTML. I'm planning to Node.js, because in the future I see as a Full-stack developer. Usually I prefer to spend my leisure time on improving my English, studying new technologies and solve tasks on Codewars. Ready to consider project work and full-time employment.  </p>
                 </div>
-                <div className={style.photo}> </div>
+                <div>
+                    <img src={photo} alt="" />
+                </div>
             </div>
 
         </div>
